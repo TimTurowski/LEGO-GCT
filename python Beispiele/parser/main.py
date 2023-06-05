81e1426261c3053be046b55126138c96de3276e1
Tim Turowski <tim.turowski@studmail.w-hs.de>
2023-06-05 21:45:37 +0200

    Bugfixes nach Besprechung mit Dennis

--- python Beispiele/parser/main.py
+++ python Beispiele/parser/main.py
@@ -1,12 +1,12 @@
 import re
 from pdfminer.high_level import extract_pages, extract_text
 
-#for page_layout in extract_pages("/Users/timturowski/Desktop/6449317.pdf"):
-#    for element in page_layout:
-#        print(element)
+# for page_layout in extract_pages("/Users/timturowski/Desktop/Bauanleitungen/10323_DE_BI_Build_Translate.pdf"):
+#     for element in page_layout:
+#         print(element)
 
-text = extract_text("/Users/timturowski/Desktop/Bauanleitungen/10323_DE_BI_Build_Translate.pdf")
-#print(text) für Textausgabe
+text = extract_text("/Users/timturowski/Desktop/Bauanleitungen/6449317.pdf")
+#print(text)
 
 class Bauteil:
     def __init__(self, anzahl, einzelteil_id):
@@ -28,7 +28,7 @@ def parse_text(text):
         if line.endswith('x') and i + 1 < num_lines:
             next_line = lines[i + 1].strip()
             if len(next_line) == 7 and next_line.isdigit():
-                anzahl = int(line[:-1])
+                anzahl = (line[:-1])
                 einzelteil_id = next_line
                 bauteil = Bauteil(anzahl, einzelteil_id)
                 legoset.bauteile.append(bauteil)
