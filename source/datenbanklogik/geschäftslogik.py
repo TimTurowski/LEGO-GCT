fce791b7aa8de7e142f541c78a9548ad51511611
DESKTOP-B5NJFTM\Caner Kara <c.kara0117@gmail.com>
2023-07-20 12:13:21 +0200

    Weitere Funktion (einspeisen) für die Datenbanklogik hinzugefügt

--- source/datenbanklogik/geschäftslogik.py
+++ source/datenbanklogik/geschäftslogik.py
@@ -29,27 +29,52 @@ def legosetpreise(legoset):
 def einzelteilpreise(einzelteil):
     session = Session()
     if isinstance(einzelteil, entities.Einzelteil):
-        result = session.query(entities.EinzelteilMarktpreis).filter(entities.EinzelteilMarktpreis.einzelteile == einzelteil).all()
+        result = session.query(entities.EinzelteilMarktpreis)\
+            .filter(entities.EinzelteilMarktpreis.einzelteile == einzelteil).all()
         if not result:
             result = "Einzelteil ist nicht in der Datenbank oder hat keine gecrawlten Preise"
     else:
         result = "Übergebenes Objekt ist kein Legoset"
     return result
 
-"""def einspeisen(entität):
+def einspeisen(entität):
     session = Session()
     result = "Objekt enspricht keinem Datenbankobjekt"
+    session.begin()
     if isinstance(entität,entities.SetMarktpreis):
-        session.query(entität)
-        if
-        session.begin()
-        session.add(entität)
-        session.commit()
-        session.close()
-        result = "War erfolgreich"
+        session.add(entities.SetMarktpreis(set_id=entität.set.set_id,anbieter_url=entität.anbieter.url,
+                                           preis=entität.preis,url=entität.url))
+        result = "Neuer SetMarktpreis wurde erfolgreich hinzugefügt"
     if isinstance(entität,entities.EinzelteilMarktpreis):
-
+        session.add(
+            entities.EinzelteilMarktpreis(einzelteil_id=entität.einzelteile.einzelteil_id,
+                                          anbieter_url=entität.anbieter.url,preis=entität.preis,url=entität.url))
+        result = "Neuer EinzelteilMarktpreis wurde erfolgreich hinzugefügt"
     if isinstance(entität,entities.EinzelteilLegoset):
-
+        einzelteil = session.query(entität.einzelteile.__class__).filter(
+            entities.Einzelteil.einzelteil_id == entität.einzelteile.einzelteil_id).all()
+        set = session.query(entität.set.__class__).filter(entities.Legoset.set_id == entität.set.set_id).all()
+        if einzelteil and set:
+            session.add(
+                entities.EinzelteilLegoset(einzelteil_id=entität.einzelteile.einzelteil_id,set_id=entität.set.set_id,
+                                           anzahl=entität.anzahl))
+            result = "Neuer EinzelteilLegoset wurde erfolgreich hinzugefügt"
+        elif einzelteil or set:
+            if einzelteil:
+                session.add(
+                    entities.EinzelteilLegoset(einzelteil_id=entität.einzelteile.einzelteil_id,anzahl=entität.anzahl,
+                                               set=entität.set))
+                result = "Neuer EinzelteilLegoset und neues Legoset wurde erfolgreich hinzugefügt"
+            else:
+                session.add(entities.EinzelteilLegoset(set_id=entität.set.set_id, anzahl=entität.anzahl,
+                                               einzelteile=entität.einzelteile))
+                result = "Neuer EinzelteilLegoset und neues Einzelteil wurde erfolgreich hinzugefügt"
+        else:
+            session.add(entities.EinzelteilLegoset(entität))
+            result = "Neuer EinzelteilLegoset, neues Einzelteil und neues Legoset wurde erfolgreich hinzugefügt"
     if isinstance(entität,entities.Anbieter):
-    return result"""
+        if not session.query(entität.__class__).filter(entities.Anbieter.url == entität.url).all():
+            session.add(entität)
+    session.commit()
+    session.close()
+    return result
--- source/datenbanklogik/herumprobieren.py
+++ source/datenbanklogik/herumprobieren.py
@@ -7,9 +7,9 @@ e2 = entities.Einzelteil(einzelteil_id="2")
 e3 = entities.Einzelteil(einzelteil_id="3")"""
 l1 = entities.Legoset(set_id="1",name="hi")
 l2 = entities.Legoset(set_id="2",name="bye")
-with Session() as session:
+"""with Session() as session:
     with session.begin():
-        """session.add(e1)
+        session.add(e1)
         session.add(e2)
         session.add(e3)
         session.add(l1)
@@ -23,13 +23,17 @@ with Session() as session:
         session.add(entities.SetMarktpreis(set_id=l2.set_id, preis=10.99, url="www.alfred.de/2",
                                            anbieter=entities.Anbieter(url="www.alfred.de", name="alfred")))
         session.add(entities.SetMarktpreis(set_id=l2.set_id,anbieter_url="www.lego.de", preis=14.99, url="www.lego.de/2"))
-        session.add(entities.SetMarktpreis(set_id=l1.set_id,anbieter_url=["www.lego.de","www.peter.com","www.alfred.de"],preis=[12.5,13.5,11.5],url=["www.lego.de/1","www.peter.com/1","www.alfred.de/1"]))"""
+        session.add(entities.SetMarktpreis(set_id=l1.set_id,anbieter_url=["www.lego.de","www.peter.com","www.alfred.de"],preis=[12.5,13.5,11.5],url=["www.lego.de/1","www.peter.com/1","www.alfred.de/1"]))
         session.add(entities.EinzelteilLegoset(anzahl=3,einzelteile=entities.Einzelteil(einzelteil_id="1"),set=entities.Legoset(set_id="3",name="peter")))
 
         session.commit()
-    session.close()
+    session.close()"""
     
 """for i in einzelteilliste():
     print(i)"""
 """for i in legosetpreise(l1):
     print(i)"""
+session = Session()
+session.begin()
+session.commit()
+session.close()
