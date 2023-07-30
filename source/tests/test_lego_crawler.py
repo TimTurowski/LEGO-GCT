4801b8431c369d0c849b42d8f04c259515b30db0
Tim Sibum <tim.sibum@studmail.w-hs.de>
2023-07-30 11:05:07 +0200

    schreiben von einem Unittest für den Lego Crawler

--- source/tests/test_lego_crawler.py
+++ source/tests/test_lego_crawler.py
@@ -1,22 +1,49 @@
+import multiprocessing
 import unittest
-from source.datastructures.einzelteil import Einzelteil
+from multiprocessing import Process
+from source.Entity.entities import Einzelteil
 from source.crawler.lego_crawler import LegoCrawler
 
+
+def execute_crawling(einzelteile, teile_crawler, conn2):
+    crawler = teile_crawler
+    result = crawler.crawl_preis(einzelteile)
+    """gibt informationen über den Crawl vorgang aus"""
+    conn2.send(result)
+
 class MyTest(unittest.TestCase):
+
     def test_valides_einzelteil(self):
-        crawler = LegoCrawler()
-        result = crawler.crawl_preis([Einzelteil("300526")])
+        """referenz Dictonary zum abgleichen der Preise mit den Preisen des Crawl results. Stand 30.07.2023"""
+        reference_dict = {"6435857": 0.14,
+                          "6406522": 0.33,
+                          "6411329": 0.1,
+                          "6390506": 1.59}
 
-        marktpreis = result.get_lego_teile()[0]
-        self.assertEqual(marktpreis.einzelteil.element_id, "300526")
-        self.assertEqual(marktpreis.preis, 0.07)
-        self.assertEqual(marktpreis.url, "https://www.lego.com/de-de/pick-and-build/pick-a-brick?query=300526")
+        sample_einzelteile = [Einzelteil(einzelteil_id="6435857"),
+                              Einzelteil(einzelteil_id="6406522"),
+                              Einzelteil(einzelteil_id="6411329"),
+                              Einzelteil(einzelteil_id="6390506"),
+                              Einzelteil(einzelteil_id="6360899")]
 
+        conn1, conn2 = multiprocessing.Pipe()
+        p = Process(target=execute_crawling, args=(sample_einzelteile, LegoCrawler(), conn2))
+        p.start()
+        p.join()
+        lego_crawl_result = conn1.recv()
+        marktpreise = lego_crawl_result.einzelteil_marktpreise
 
+        """generiert aus Liste von marktpreisen ein Dict"""
+        result_dict = {}
+        for i in marktpreise:
+            result_dict[i.einzelteile.einzelteil_id] = i.preis
 
+        self.assertEqual(reference_dict, result_dict)
 
 
 if __name__ == '__main__':
+
     unittest.main()
+
 
 
