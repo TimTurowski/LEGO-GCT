38b53bd764156beae56f2b8faf35ca03a6000bad
Tim Sibum <tim.sibum@studmail.w-hs.de>
2023-10-31 12:09:27 +0100

    update_part_prices.py für den Server anpassen

--- source/geschaeftslogik/update_part_prices.py
+++ source/geschaeftslogik/update_part_prices.py
@@ -7,7 +7,7 @@ from source.Entity.entities import Einzelteil
 from source.datenbanklogik.datenzugriffsobjekt import Datenzugriffsobjekt
 
 """funktion zum Ausführen der Crawling logik"""
-def execute_crawling(einzelteile, teile_crawler):
+def execute_crawling(einzelteile, teile_crawler, anbieter):
     crawler = teile_crawler
     result = crawler.crawl_preis(einzelteile)
     print(result.failed_lego_teile)
@@ -17,23 +17,32 @@ def execute_crawling(einzelteile, teile_crawler):
 
     """löscht alle Marktpreise aus der DB, welche nicht mehr verfügbar sind"""
     print(result.failed_lego_teile)
-    dao.remove_einzelteil_marktpreise(result.failed_lego_teile, "https://www.toypro.com")
+    dao.remove_einzelteil_marktpreise(result.failed_lego_teile, anbieter)
 
 
 if __name__ == "__main__":
+    """Defaultwerte für Crawler"""
+    shop_url = "https://www.toypro.com"
+    shop_crawler = ToyproCrawler()
     dao = Datenzugriffsobjekt()
 
-    print(len(dao.einzelteil_ohne_marktpreis("https://www.toypro.com")))
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "toypro":
+            shop_url = "https://www.toypro.com"
+            shop_crawler = ToyproCrawler()
+        elif sys.argv[1] == "lego":
+            shop_url = "https://www.lego.com/de-de/pick-and-build/pick-a-brick"
+            shop_crawler = LegoCrawler()
 
 
 
-
-    marktpreise = dao.einzelteil_marktpreis_liste("https://www.toypro.com")[0:10]
+    marktpreise = dao.einzelteil_marktpreis_liste(shop_url)
 
     einzelteile = list(map(lambda a: Einzelteil(einzelteil_id=a[0]), marktpreise))
 
-    print(len(einzelteile))
-    p = Process(target=execute_crawling, args=(einzelteile, ToyproCrawler()))
+    execute_crawling(einzelteile, shop_crawler, shop_url)
+    # p = Process(target=execute_crawling, args=(einzelteile, shop_crawler))
     # # 6437883 0.86
-    p.start()
+    # p.start()
 
